e94cc9c0cfe65f83e03f6d6c5494cf06b964b720
Rob Curcio <robby.curcio@gmail.com>
2020-03-19 02:49:46 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,47 @@
+const form = document.getElementById('addForm');
+const itemList = document.getElementById('items');
+const filter = document.getElementById('filter');
+
+form.addEventListener('submit', addItem);
+itemList.addEventListener('click', removeItem);
+filter.addEventListener('keyup', filterItem);
+
+function addItem(e) {
+    e.preventDefault();
+
+    const newItem = document.getElementById('item').value;
+
+    const  li = document.createElement('li');
+    li.className = 'list-group-item';
+    li.appendChild(document.createTextNode(newItem));
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.className = 'btn btn-danger btn-md float-right delete';
+    deleteBtn.appendChild(document.createTextNode('x'));
+
+    li.appendChild(deleteBtn);
+
+    itemList.appendChild(li);
+}
+
+function removeItem(e) {
+    if (e.target.classList.contains('delete')) {
+        if (confirm('Are you sure?')) {
+            let li = e.target.parentElement;
+            itemList.removeChild(li);
+        }
+    }
+}
+
+function filterItem(e) {
+    const text = e.target.value.toLowerCase();
+    const items = itemList.getElementsByTagName('li');
+    Array.from(items).forEach(function(item) {
+        let itemName = item.firstChild.textContent;
+        if (itemName.toLowerCase().indexOf(text) != -1) {
+            item.style.display = 'block';
+        } else {
+            item.style.display = 'none';
+        }
+    });
+}
